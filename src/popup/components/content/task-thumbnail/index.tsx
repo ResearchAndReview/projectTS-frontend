b746db5b8f77ee465daba272e67e554f933f18bd
yooshnn <yooshnn@kakao.com>
2025-06-03 01:34:24 +0900

    feat(popup): open image on click

--- src/popup/components/content/task-thumbnail/index.tsx
+++ src/popup/components/content/task-thumbnail/index.tsx
@@ -1,5 +1,7 @@
+import { useCallback } from 'react';
 import { Task } from '@/types';
 import { Badge, Overlay, ThumbnailImg, Wrapper } from './styles';
+import { openImage } from './utils';
 
 interface Props {
   task: Task;
@@ -8,14 +10,11 @@ interface Props {
 export const TaskThumbnail = ({ task }: Props) => {
   const { id, image, status } = task;
 
-  const handleClick = () => {
-    console.log('Clicked task:', id);
-  };
-
   const showBadge = status === 'pending' || status === 'error';
+  const clickHandler = useCallback(() => openImage(image), [image]);
 
   return (
-    <Wrapper onClick={handleClick}>
+    <Wrapper onClick={clickHandler}>
       <ThumbnailImg src={image} alt={`task-${id}`} />
       {showBadge && <Overlay />}
       {showBadge && <Badge status={status} />}
